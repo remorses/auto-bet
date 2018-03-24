51dcb6a4dbcc9a30d7f43e44973cadb92ae017c2
morse <beats.by.morse@gmail.com>
2018-03-24 12:07:58 +0100

    aliases ora funziona, c'era un array in più

--- src/aliases.ts
+++ src/aliases.ts
@@ -4,19 +4,32 @@
 
 
 const aliases = {
-  oddType: {
-    "underOver_2.5": [{ betfair: "Più/Meno", williamhill: "Over/Under 2.5 Goal" }],
-    "rimborso pareggio": [{ betfair: "Rimborso in Caso di Pareggio" }]
+  "oddType": {
+    "underOver_2.5":
+      {
+        betfair: "Più/Meno",
+        williamhill: "Over/Under 2.5 Goal"
+      }
+    ,
+    "rimborso pareggio":
+      {
+        betfair: "Rimborso in Caso di Pareggio"
+      }
+
   },
-  tournament: {}
+  "tournament": {
+
+  }
 }
 
 type Category = "oddType" | "tournament";
 
 const rawToPure = (category: Category, raw: string): string => {
-  for (let [group, arr] of Object.entries(aliases[category])) {
-    for (let value of Object.values(group)) {
-      if (value === raw.trim()) return value
+  for (let [key, obj] of Object.entries(aliases[category])) {
+    console.log("obj ", obj)
+    for (let alias of Object.values(obj)) {
+      console.log("alias ", alias)
+      if (alias == raw.trim()) return key
     }
   }
   return raw
