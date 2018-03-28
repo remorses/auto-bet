e5e7e65995748e7867a8b81f0936816ea857018c
morse <beats.by.morse@gmail.com>
2018-03-28 17:49:02 +0200

    tolti i try catch dagli helpers

--- src/scraper/helpers.ts
+++ src/scraper/helpers.ts
@@ -1,20 +1,16 @@
 import { Page, ElementHandle, JSHandle, Browser, Request, Response } from 'puppeteer'
-import { when, observable, action, reaction } from "mobx"
-import * as Parallel from "async-parallel"
-
+// import { when, observable, action, reaction } from "mobx"
 
 
 // invece di waitFor
-const waitForLoad = (page: Page) => new Promise((resolve) => {
+export const waitForLoad = (page: Page) => new Promise((resolve) => {
   page.on('request', (req) => {
     setTimeout(() => resolve("timeOut"), 500)
   })
   setTimeout(() => resolve("timeOut"), 2500)
 })
 
-
-
-
+/*
 // mobx
 // XXX solo per la modalità live, ogni volta che arrivano le nuove quote
 // eseguo lo scrape. Devo anche separare la funzione scrape da new page, dentro il map().
@@ -28,15 +24,10 @@ function whenUpdated(page) {
     }
   })
   reaction(() => updates.value, (value) => console.log(value))
-}
-
-
-
-
-
+}*/
 
 // get child content
-const getChildContent = async ({ page, parent, selector }) => {
+export const getChildContent = async ({ page, parent, selector }) => {
   let results: string[] = []
   const elements: ElementHandle[] = await parent.$$(selector)
   if (elements.length < 1) throw new Error("can't proceed, no elements")
@@ -49,108 +40,92 @@ const getChildContent = async ({ page, parent, selector }) => {
 }
 
 
-
-
-
-
-const findParentElement = async ({ page, content, child, parent }: { page: Page, content: string, child: string, parent: string }) => {
+export const findParentElement = async ({ page, content, child, parent }: { page: Page, content: string, child: string, parent: string }) => {
   let result
-  try {
-    await page.waitForSelector(parent + " " + child)
-    // console.log(parent + child)
-    const elements: ElementHandle[] = await page.$$(parent)
-    if (elements.length < 1) throw new Error("can't proceed, no elements")
-    for (let element of elements) {
-      let childElement = await element.$(child) // null TODO
-      if (!childElement) continue
-      let inner = await getContent(childElement)
-      console.log("find parent: ", inner, )
-      if (inner.trim() === content) { result = element; break } else { continue }
-    }
-  } catch (e) { console.error("findParentElement: ", e) }
+  await page.waitForSelector(parent + " " + child)
+  // console.log(parent + child)
+  const elements: ElementHandle[] = await page.$$(parent)
+  if (elements.length < 1) throw new Error("can't proceed, no elements")
+  for (let element of elements) {
+    let childElement = await element.$(child) // null TODO
+    if (!childElement) continue
+    let inner = await getContent(childElement)
+    console.log("find parent: ", inner, )
+    if (inner.trim() === content) { result = element; break } else { continue }
+  }
   return result
 }
 
 
-
-export const findParentElementFromElement = async ({ page, grandParent, content, child, parent }: { page: Page,grandParent: ElementHandle,  content: string, child: string, parent: string }) => {
+export const findParentElementFromElement = async ({ page, grandParent, content, child, parent }: { page: Page, grandParent: ElementHandle, content: string, child: string, parent: string }) => {
   let result
-  try {
-    // console.log(parent + child)
-    const elements: ElementHandle[] = await grandParent.$$(parent)
-    if (elements.length < 1) throw new Error("can't proceed, no elements")
-    for (let element of elements) {
-      let childElement = await element.$(child) // null TODO
-      if (!childElement) continue
-      let inner = await getContent(childElement)
-      console.log("find parent: ", inner, )
-      if (inner.trim() === content) { result = element; break } else { continue }
-    }
-  } catch (e) { console.error("findParentElement: ", e) }
+  // console.log(parent + child)
+  const elements: ElementHandle[] = await grandParent.$$(parent)
+  if (elements.length < 1) throw new Error("findParentElementFromElement: can't proceed, no elements")
+  for (let element of elements) {
+    let childElement = await element.$(child) // null TODO
+    if (!childElement) continue
+    let inner = await getContent(childElement)
+    console.log("find parent: ", inner, )
+    if (inner.trim() === content) { result = element; break } else { continue }
+  }
   return result
 }
 
 
 // XXX returns children of an <element> with a <selector>
-const getChildren = async ({ page, element, selector }: { page: Page, element: JSHandle, selector: string }): Promise<ElementHandle[]> => {
+export const getChildren = async ({ page, element, selector }: { page: Page, element: JSHandle, selector: string }): Promise<ElementHandle[]> => {
   let children: ElementHandle[] = [];
-  try {
-    if (!element) throw new Error("can't proceed getChildren(), no element")
-    const listHandle = await page.evaluateHandle((element, selector) => element.querySelectorAll(selector), element, selector);
-    if (!listHandle) throw new Error("can't proceed getChildren(), no children")
-    const properties = await listHandle.getProperties();
-    for (const property of properties.values()) {
-      const child = property.asElement();
-      if (child) {
-        children.push(child);
-        console.log(await getContent(child), " ,get children")
-      }
+  if (!element) throw new Error("can't proceed getChildren(), no element")
+  const listHandle = await page.evaluateHandle((element, selector) => element.querySelectorAll(selector), element, selector);
+  if (!listHandle) throw new Error("can't proceed getChildren(), no children")
+  const properties = await listHandle.getProperties();
+  for (const property of properties.values()) {
+    const child = property.asElement();
+    if (child) {
+      children.push(child);
+      console.log(await getContent(child), " ,get children")
     }
-  } catch (e) { console.log(e) }
+  }
   return children;
 }
 
 
-
-
-
-const findElement = async ({ page, content, selector }: { page: Page, content: string, selector: string }) => {
-  try {
-    await page.waitForSelector(selector)
-    const elements: ElementHandle[] = await page.$$(selector)
-    for (let element of elements) {
-      let inner = await getContent(element)
-      //  console.log(inner.trim())
-      if (inner.trim() === content) { console.log(inner, ", findElement"); return element }
-    }
-  } catch (e) { console.log(e) }
+export const findElement = async ({ page, content, selector }: { page: Page, content: string, selector: string }) => {
+  await page.waitForSelector(selector)
+  const elements: ElementHandle[] = await page.$$(selector)
+  for (let element of elements) {
+    let inner = await getContent(element)
+    //  console.log(inner.trim())
+    if (inner.trim() === content) { console.log(inner, ", findElement"); return element }
+  }
 }
 
 
-
-
-const getContent = async (element: ElementHandle): Promise<string> => {
+export const getContent = async (element: ElementHandle): Promise<string> => {
   if (!element) throw new Error("getContent: no element where get the content")
   return (await (await element.getProperty("innerHTML")).jsonValue()).trim()
 }
 
-const parseChildren = (arr: ElementHandle[]) => {
+
+export const parseChildren = (arr: ElementHandle[]) => {
   return Promise.all(arr.map(el => getContent(el)))
 }
 
-const getHref = async (element: ElementHandle) => {
-  try {
-    await (await element.getProperty("href")).jsonValue()
-  } catch (e) { console.error(e) }
+
+export const getHref = async (element: ElementHandle) => {
+  await (await element.getProperty("href")).jsonValue()
 }
 
-const logger = (text) => (a) => {
+
+export const logger = (text) => (a) => {
   console.log(text, " : ", a);
   return a
 }
 
+
 // XXX
-const resolveIf = async (bool: any) => {
+export const resolveIf = async (bool: any) => {
   if (bool) { return bool }
   else {
     setTimeout(async () => { return bool }, 500)
@@ -158,15 +133,13 @@ const resolveIf = async (bool: any) => {
 }
 
 
-
-
-const getAttribute = async (page: Page, element: ElementHandle, attribute): Promise<JSHandle> => {
+export const getAttribute = async (page: Page, element: ElementHandle, attribute): Promise<JSHandle> => {
   const value = await page.evaluateHandle((element, attribute) => element.attribute, element, attribute);
   return value
 }
 
 
-const abortMediaRequests = async (page) => {
+export const abortMediaRequests = async (page) => {
   await page.setRequestInterception(true);
   page.on('request', req => {
     if (req.resourceType() === "media" || req.resourceType() === "image")
@@ -174,21 +147,4 @@ const abortMediaRequests = async (page) => {
     else
       req.continue();
   });
-}
-
-
-export {
-  getAttribute,
-  logger,
-  resolveIf,
-  getHref,
-  parseChildren,
-  getContent,
-  whenUpdated,
-  getChildContent,
-  findParentElement,
-  waitForLoad,
-  getChildren,
-  findElement,
-  abortMediaRequests
 }
